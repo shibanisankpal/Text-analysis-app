89ac0bcbc604f5a484384689d3fd81b2308666c9
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-09 01:14:32 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -1,37 +1,40 @@
 import streamlit as st
+import requests
 
-def sentiment_analysis(text):
-    positive_keywords = ['good', 'great', 'excellent', 'awesome', 'amazing', 'happy', 'joyful',
-                         'pleased', 'satisfied', 'fantastic', 'wonderful', 'love', 'like', 'best',
-                         'beautiful', 'fun', 'nice', 'exciting']
+def download_afinn():
+    url = "https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-111.txt"
+    response = requests.get(url)
+    afinn_data = response.text
+    afinn = dict()
+    for line in afinn_data.split("\n"):
+        word, score = line.split("\t")
+        afinn[word] = int(score)
+    return afinn
 
-    negative_keywords = ['bad', 'terrible', 'poor', 'awful', 'horrible', 'sad', 'disappointed',
-                         'frustrated', 'unhappy', 'displeased', 'miserable', 'angry', 'hate',
-                         'worst', 'ugly', 'annoying', 'disgusting', 'unpleasant', 'horrid']
-
+def sentiment_analysis(text, afinn):
     words = text.lower().split()
+    sentiment_score = sum(afinn.get(word, 0) for word in words)
 
-    positive_count = sum(keyword in words for keyword in positive_keywords)
-    negative_count = sum(keyword in words for keyword in negative_keywords)
-
-    if positive_count > negative_count:
+    if sentiment_score > 0:
         return "Positive"
-    elif positive_count < negative_count:
+    elif sentiment_score < 0:
         return "Negative"
     else:
         return "Neutral"
 
 def main():
     st.title("NLP Tool: Sentiment Analysis")
+    afinn = download_afinn()
 
     user_text = st.text_area("Enter text:", "")
 
     if st.button("Analyze"):
         if user_text.strip() != "":
-            result = sentiment_analysis(user_text)
+            result = sentiment_analysis(user_text, afinn)
             st.write("Sentiment:", result)
         else:
             st.warning("Please enter some text.")
 
 if __name__ == "__main__":
     main()
+
