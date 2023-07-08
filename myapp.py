8d0441a29221ab6832d1ef93a62a28181baa70d7
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-09 01:12:04 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -1,10 +1,25 @@
 import streamlit as st
-from transformers import pipeline
 
 def sentiment_analysis(text):
-    classifier = pipeline("sentiment-analysis")
-    result = classifier(text)[0]
-    return result["label"]
+    positive_keywords = ['good', 'great', 'excellent', 'awesome', 'amazing', 'happy', 'joyful',
+                         'pleased', 'satisfied', 'fantastic', 'wonderful', 'love', 'like', 'best',
+                         'beautiful', 'fun', 'nice', 'exciting']
+
+    negative_keywords = ['bad', 'terrible', 'poor', 'awful', 'horrible', 'sad', 'disappointed',
+                         'frustrated', 'unhappy', 'displeased', 'miserable', 'angry', 'hate',
+                         'worst', 'ugly', 'annoying', 'disgusting', 'unpleasant', 'horrid']
+
+    words = text.lower().split()
+
+    positive_count = sum(keyword in words for keyword in positive_keywords)
+    negative_count = sum(keyword in words for keyword in negative_keywords)
+
+    if positive_count > negative_count:
+        return "Positive"
+    elif positive_count < negative_count:
+        return "Negative"
+    else:
+        return "Neutral"
 
 def main():
     st.title("NLP Tool: Sentiment Analysis")
